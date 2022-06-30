8245766003845e6707405a4fa22ccafc78c2b39d
Silvan Zahno <silvan.zahno@hevs.ch>
2022-06-30 09:17:32 +0000

    2022-06-30 09:17:28: Automatic Library Update with deployLibs.bash :shipit:

--- Libs/UVM/hdl/parallelOutDriver_sim.vhd
+++ Libs/UVM/hdl/parallelOutDriver_sim.vhd
@@ -11,15 +11,15 @@ ARCHITECTURE RTL OF parallelOutDriver IS
 BEGIN
   ------------------------------------------------------------------------------
                                                         -- interpret transaction
-  interpretTransaction: process(driverTransaction)
+  interpretTransaction: process(transaction)
     variable myLine : line;
     variable commandPart : line;
   begin
-    write(myLine, driverTransaction);
+    write(myLine, transaction);
     rm_side_separators(myLine);
     read_first(myLine, commandPart);
     if commandPart.all = command then
-      parallelOut_int <= sscanf(myLine.all);
+      parallelOut_int <= sscanf(myLine.all)(parallelOut_int'range);
     end if;
     deallocate(myLine);
   end process interpretTransaction;
